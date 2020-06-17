2427fa8e11fd319088ab3f39d27409456e18609d
Alex <fusy123@mail.ru>
2020-06-17 08:44:36 +0300

    подсчет длительности песен

--- lesson_002/06_songs.py
+++ lesson_002/06_songs.py
@@ -21,7 +21,8 @@ violator_songs = [
 # где a, это число которое надо округлить, а b количество знаков после запятой
 # более подробно про функцию round смотрите в документации https://docs.python.org/3/search.html?q=round
 
-# TODO здесь ваш код
+time_songs=round(violator_songs[3][1]+violator_songs[5][1]+violator_songs[-1][1], 2)
+
 
 # Есть словарь песен группы Yellow со временем звучания с точностью до долей минут
 pocket_universe_songs = {
@@ -41,7 +42,9 @@ pocket_universe_songs = {
 # Распечатайте общее время звучания трех песен: 'On Track', 'To the Sea' и 'Beyond Mirrors'
 #   А другие три песни звучат приблизительно ХХХ минут
 
-# TODO здесь ваш код
+other_songs=round(pocket_universe_songs['On Track']+pocket_universe_songs['To the Sea']+pocket_universe_songs['Beyond Mirrors'], 2)
+
+print ('Три песни звучат ', time_songs, 'минут','\n''А другие три песни звучат приблизительно ', other_songs, 'минут')
 
 # Обратите внимание, что делать много вычислений внутри print() - плохой стиль.
 # Лучше заранее вычислить необходимое, а затем в print(xxx, yyy, zzz)
