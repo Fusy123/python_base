35c01595c4b88759dfbbefad271359d1f445110c
Alex <fusy123@mail.ru>
2020-06-22 14:37:25 +0300

    color bubbles

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -1,23 +1,52 @@
 # -*- coding: utf-8 -*-
+import random
 
 import simple_draw as sd
 
 sd.resolution = (1200, 600)
 
 # Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
-# TODO здесь ваш код
+# TODO почему когда указываю color=(150, 24,120) цвет меняется, а если указывать color=COLOR_RED выпадает ошибка?
+# sd.circle(center_position=point, radius=radius, color=COLOR_RED, width=3)
+# NameError: name 'COLOR_RED' is not defined
 
-# Написать функцию рисования пузырька, принммающую 3 (или более) параметра: точка рисования, шаг и цвет
-# TODO здесь ваш код
+# point=sd.get_point(600, 300)
+# radius=50
+# for _ in range(3):
+#     sd.circle(center_position=point, radius=radius, color=(150, 24,120), width=3)
+#     radius+=5
+
+
+def bubble(point, step):
+    radius=50
+    for _ in range(3):
+        color=sd.random_color()
+        radius += step
+        sd.circle(center_position=point, radius=radius, color=color, width=2)
+
+# point=sd.get_point(300, 300)
+# bubble(point=point, step=10)
+
 
 # Нарисовать 10 пузырьков в ряд
-# TODO здесь ваш код
+
+# for x in range(100, 1001, 100):
+#     point = sd.get_point(x, 300)
+#     bubble(point=point, step=5)
 
 # Нарисовать три ряда по 10 пузырьков
-# TODO здесь ваш код
+# for y in range (100, 301, 50):
+#     for x in range(100, 1001, 50):
+#         point = sd.get_point(x, y)
+#         bubble(point=point, step=5)
+
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
-# TODO здесь ваш код
+
+for _ in range(100):
+    point=sd.random_point()
+    step=random.randint(2,10)
+    bubble(point=point, step=step)
 
 sd.pause()
 
