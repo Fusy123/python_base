9fe02de82cd331e46f445509ef14c8880a700a5a
Alex <fusy123@mail.ru>
2020-06-22 22:34:49 +0300

    rainbow

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -3,18 +3,37 @@
 # (цикл for)
 
 import simple_draw as sd
+sd.resolution = (1200, 600)
 
 rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
-# TODO здесь ваш код
+
+x=50
+y=50
+x1=1100
+y1=1100
+for lines in rainbow_colors:
+    x+=5
+    x1+=5
+    start_point = sd.get_point(x, 50)
+    end_point = sd.get_point(x1, 450)
+    sd.line(start_point=start_point, end_point=end_point, color=lines, width=4)
+
+
 # Подсказка: цикл нужно делать сразу по тьюплу с цветами радуги.
 
 
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
-# TODO здесь ваш код
 
+point=sd.get_point(600, -60)
+def bubble(point, step):
+    radius=340
+    for lines in rainbow_colors:
+        radius += step
+        sd.circle(center_position=point, radius=radius, color=lines, width=40)
+bubble(point, 40)
 sd.pause()
