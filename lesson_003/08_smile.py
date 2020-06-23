c4a7edcf824e3eb704af0ce8df9ec9615cc9da00
Alex <fusy123@mail.ru>
2020-06-23 11:26:16 +0300

    smiles

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -1,13 +1,38 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
+import simple_draw as sd
 
+
+sd.resolution = (1200, 600)
 # Написать функцию отрисовки смайлика по заданным координатам
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
 
-# TODO здесь ваш код
+def smile(point, step):
+    radius = step
+    color = sd.random_color()
+    sd.circle(center_position=point, radius=radius, color=color, width=0)
+    # рисуем глаза
+    left_eye = sd.get_point(x - 20, y + 13)
+    right_eye = sd.get_point(x + 20, y + 13)
+    sd.circle(left_eye, radius=6, color=(0,0,0), width=0)
+    sd.circle(right_eye, radius=6, color=(0,0,0), width=0)
+    # рисуем нос
+    nose_start=sd.get_point(x, y+15)
+    nose_finish=sd.get_point(x, y-7)
+    sd.line(nose_start, nose_finish, color=(0,0,0), width=2)
+    #рисуем рот
+    point_list=[sd.get_point(x-28, y-20), sd.get_point(x, y-25), sd.get_point(x+28, y-20)]
+    sd.polygon(point_list=point_list, color=(0,0,0), width=0)
 
-simple_draw.pause()
+for _ in range(10):
+    x=sd.random_number(100, 1200)
+    y=sd.random_number(100, 600)
+    point=sd.get_point(x, y)
+    step=sd.random_number(40, 60)
+    smile(point=point, step=step)
+
+
+sd.pause()
