3b6aeed95d2212a93b3a4b2c1e2a1a4177c2261b
Alex <fusy123@mail.ru>
2020-06-25 09:37:08 +0300

    форматирование по PEP8

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -10,11 +10,11 @@ sd.resolution = (1200, 600)
 # sd.circle(center_position=point, radius=radius, color=COLOR_RED, width=3)
 # NameError: name 'COLOR_RED' is not defined
 
-point=sd.get_point(600, 300)
-radius=50
+point = sd.get_point(600, 300)
+radius = 50
 for _ in range(3):
-    sd.circle(center_position=point, radius=radius, color=(150, 24,120), width=3)
-    radius+=5
+    sd.circle(center_position=point, radius=radius, color=(150, 24, 120), width=3)
+    radius += 5
 
 
 def bubble(point, step):
@@ -24,28 +24,28 @@ def bubble(point, step):
         radius += step
         sd.circle(center_position=point, radius=radius, color=color, width=2)
 
-# point=sd.get_point(300, 300)
+# point = sd.get_point(300, 300)
 # bubble(point=point, step=10)
 
 
 # Нарисовать 10 пузырьков в ряд
 
-# for x in range(100, 1001, 100):
-#     point = sd.get_point(x, 300)
-#     bubble(point=point, step=5)
+for x in range(100, 1001, 100):
+    point = sd.get_point(x, 300)
+    bubble(point=point, step=5)
 
 # Нарисовать три ряда по 10 пузырьков
-# for y in range (100, 301, 50):
-#     for x in range(100, 1001, 50):
-#         point = sd.get_point(x, y)
-#         bubble(point=point, step=5)
+for y in range(100, 301, 50):
+    for x in range(100, 1001, 50):
+        point = sd.get_point(x, y)
+        bubble(point=point, step=5)
 
 
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
 
 for _ in range(100):
-    point=sd.random_point()
-    step=random.randint(3,12)
+    point = sd.random_point()
+    step = random.randint(3, 12)
     bubble(point=point, step=step)
 
 sd.pause()
