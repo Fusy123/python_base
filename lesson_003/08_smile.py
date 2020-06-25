3d3ca4d714e42d205d5bce69d7e2c8bf2b764ee7
Alex <fusy123@mail.ru>
2020-06-25 09:47:55 +0300

    форматирование по PEP8

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -17,21 +17,21 @@ def smile(point, step):
     # рисуем глаза
     left_eye = sd.get_point(x - 20, y + 13)
     right_eye = sd.get_point(x + 20, y + 13)
-    sd.circle(left_eye, radius=6, color=(0,0,0), width=0)
-    sd.circle(right_eye, radius=6, color=(0,0,0), width=0)
+    sd.circle(left_eye, radius=6, color=(0, 0, 0), width=0)
+    sd.circle(right_eye, radius=6, color=(0, 0, 0), width=0)
     # рисуем нос
-    nose_start=sd.get_point(x, y+15)
-    nose_finish=sd.get_point(x, y-7)
-    sd.line(nose_start, nose_finish, color=(0,0,0), width=2)
+    nose_start = sd.get_point(x, y+15)
+    nose_finish = sd.get_point(x, y-7)
+    sd.line(nose_start, nose_finish, color=(0, 0, 0), width=2)
     #рисуем рот
-    point_list=[sd.get_point(x-28, y-20), sd.get_point(x, y-25), sd.get_point(x+28, y-20)]
-    sd.polygon(point_list=point_list, color=(0,0,0), width=0)
+    point_list = [sd.get_point(x-28, y-20), sd.get_point(x, y-25), sd.get_point(x+28, y-20)]
+    sd.polygon(point_list=point_list, color=(0, 0, 0), width=0)
 
 for _ in range(10):
-    x=sd.random_number(100, 1200)
-    y=sd.random_number(100, 600)
-    point=sd.get_point(x, y)
-    step=sd.random_number(40, 60)
+    x = sd.random_number(100, 1200)
+    y = sd.random_number(100, 600)
+    point = sd.get_point(x, y)
+    step = sd.random_number(40, 60)
     smile(point=point, step=step)
 
 
