51e223909d932f68eb93d59ea6661d4857e4714c
Alex <fusy123@mail.ru>
2020-06-30 23:27:11 +0300

    reformat PEP8

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -3,27 +3,21 @@
 # (цикл for)
 
 import simple_draw as sd
+
 sd.resolution = (1200, 600)
 
 rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
                   sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
-
-# TODO Есть недочеты в форматировании по PEP8, используйте пункт меню в пайчарме
-# TODO Ненужные переменные удаляем из кода
-x=50
-y=50
-x1=1100
-y1=1100
-# TODO Мы получаем не линии а цвет, так и назовем переменную
-for lines in rainbow_colors:
-    x+=5
-    x1+=5
+x = 50
+x1 = 350
+for color in rainbow_colors:
+    x += 5
+    x1 += 5
     start_point = sd.get_point(x, 50)
     end_point = sd.get_point(x1, 450)
-    sd.line(start_point=start_point, end_point=end_point, color=lines, width=4)
-
+    sd.line(start_point=start_point, end_point=end_point, color=color, width=4)
 
 # Подсказка: цикл нужно делать сразу по тьюплу с цветами радуги.
 
@@ -31,13 +25,15 @@ for lines in rainbow_colors:
 # Усложненное задание, делать по желанию.
 # Нарисовать радугу дугами от окружности (cсм sd.circle) за нижним краем экрана,
 # поэкспериментировать с параметрами, что бы было красиво
-# TODO Есть недочеты в форматировании по PEP8, используйте пункт меню в пайчарме
 point = sd.get_point(600, -60)
+
+
 def bubble(point, step):
     radius = 380
-    # TODO Мы получаем не линии а цвет, так и назовем переменную
-    for lines in rainbow_colors:
+    for color in rainbow_colors:
         radius += step
-        sd.circle(center_position=point, radius=radius, color=lines, width=40)
+        sd.circle(center_position=point, radius=radius, color=color, width=40)
+
+
 bubble(point, 40)
 sd.pause()
