34da0d4d498f89fca36c5efb7b2e29bd706d7576
Alex <fusy123@mail.ru>
2020-06-30 23:03:30 +0300

    reformat PEP8

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -3,14 +3,14 @@
 # (определение функций)
 import simple_draw as sd
 
-
 sd.resolution = (1200, 600)
+
+
 # Написать функцию отрисовки смайлика по заданным координатам
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
 
-# TODO Есть недочеты в форматировании по PEP8, используйте пункт меню в пайчарме
 def smile(point, step):
     radius = step
     color = sd.random_color()
@@ -21,12 +21,13 @@ def smile(point, step):
     sd.circle(left_eye, radius=6, color=(0, 0, 0), width=0)
     sd.circle(right_eye, radius=6, color=(0, 0, 0), width=0)
     # рисуем нос
-    nose_start = sd.get_point(x, y+15)
-    nose_finish = sd.get_point(x, y-7)
+    nose_start = sd.get_point(x, y + 15)
+    nose_finish = sd.get_point(x, y - 7)
     sd.line(nose_start, nose_finish, color=(0, 0, 0), width=2)
-    #рисуем рот
-    point_list = [sd.get_point(x-28, y-20), sd.get_point(x, y-25), sd.get_point(x+28, y-20)]
+    # рисуем рот
+    point_list = [sd.get_point(x - 28, y - 20), sd.get_point(x, y - 25), sd.get_point(x + 28, y - 20)]
     sd.polygon(point_list=point_list, color=(0, 0, 0), width=0)
+
 
 for _ in range(10):
     x = sd.random_number(100, 1200)
@@ -34,6 +35,5 @@ for _ in range(10):
     point = sd.get_point(x, y)
     step = sd.random_number(40, 60)
     smile(point=point, step=step)
-
 
 sd.pause()
