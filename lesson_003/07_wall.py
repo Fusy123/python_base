cbcfac9ace6021854dc83f17335c93a7a2b19931
Alex <fusy123@mail.ru>
2020-07-03 16:39:54 +0300

    переделал цикл и функцию

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -5,8 +5,7 @@ import simple_draw as sd
 
 sd.resolution = (1200, 600)
 sd.background_color = (127, 63, 0)
-
-
+color=sd.COLOR_WHITE
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 # Подсказки:
@@ -19,20 +18,16 @@ sd.background_color = (127, 63, 0)
 #           вычисляем правый нижний и левый верхний углы кирпича
 #           рисуем кирпич
 
-def draw_briks(start, finish, color, width):
+def draw_briks(start, finish):
     for x in range(start, finish, 100):
         start_point = sd.get_point(x, y)
         end_point = sd.get_point(x + 100, y + 50)
-        sd.rectangle(start_point, end_point, color=color, width=width)
+        sd.rectangle(start_point, end_point, color=color, width=2)
 
-
-# TODO Чтобы в цикле сразу получать номер ряда и Y используйте функцию enumerate(range(0, 800, 50))
-# TODO  А если мы Y не используем пишем _, и заводим цикл по количеству рядов range(11)
-for y in enumerate(range(0, 800, 50)):
-    if y % 2 != 0:
-        # TODO Используем константы из библиотеке
-        draw_briks(0, 1200, (255, 255, 255), 2)
+for a, y in enumerate(range(0, 800, 50)):
+    if a % 2 != 0:
+        draw_briks(0, 1200)
     else:
-        draw_briks(50, 1200, (255, 255, 255), 2)
+        draw_briks(50, 1200)
 
 sd.pause()
