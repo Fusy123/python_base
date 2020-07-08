42ad12a903d7aee35ccdf4998878770608794137
Alex <fusy123@mail.ru>
2020-07-08 23:16:16 +0300

    рисуем треугольник, квадрат, пятиугольник, шестиугольник

--- lesson_004/01_shapes.py
+++ lesson_004/01_shapes.py
@@ -2,6 +2,8 @@
 
 import simple_draw as sd
 
+sd.resolution = (1200, 600)
+
 # Часть 1.
 # Написать функции рисования равносторонних геометрических фигур:
 # - треугольника
@@ -36,7 +38,74 @@ import simple_draw as sd
 # sd.line()
 # Результат решения см lesson_004/results/exercise_01_shapes.jpg
 
-# TODO здесь ваш код
+#----------------------------------------------------------------------------------------
+# рисуем треугольник
+angle = 0
+length = 100
+start_point_triangle = sd.get_point(100, 100)
+start_point_quadrate = sd.get_point(200, 300)
+start_point_pentagan = sd.get_point(600, 100)
+start_point_hexagon = sd.get_point(700, 300)
+
+
+def triangle(point, angle, length):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw()
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 120, length=length, width=3)
+    v2.draw()
+    v3 = sd.line(start_point=point, end_point=v2.end_point, width=3)
+
+
+triangle(point=start_point_triangle, angle=angle, length=length)
+
+#-----------------------------------------------------------------------------------------------------
+# рисуем квадрат
+def quadrate(point, angle, length):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw()
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 90, length=length, width=3)
+    v2.draw()
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 180, length=length, width=3)
+    v3.draw()
+    v4 = sd.line(start_point=point, end_point=v3.end_point, width=3)
+
+
+quadrate(point=start_point_quadrate, angle=angle, length=length)
+
+#------------------------------------------------------------------------------------------------------
+# рисуем пятиугольник
+def pentagan(point, angle, length):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw()
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 72, length=length, width=3)
+    v2.draw()
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 144, length=length, width=3)
+    v3.draw()
+    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 216, length=length, width=3)
+    v4.draw()
+    v5 = sd.line(start_point=point, end_point=v4.end_point, width=3)
+
+
+pentagan(point=start_point_pentagan, angle=angle, length=length)
+
+#---------------------------------------------------------------------------------------------------------
+# рисуем шестиугольник
+def hexagon(point, angle, length):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length, width=3)
+    v1.draw()
+    v2 = sd.get_vector(start_point=v1.end_point, angle=angle + 60, length=length, width=3)
+    v2.draw()
+    v3 = sd.get_vector(start_point=v2.end_point, angle=angle + 120, length=length, width=3)
+    v3.draw()
+    v4 = sd.get_vector(start_point=v3.end_point, angle=angle + 180, length=length, width=3)
+    v4.draw()
+    v5 = sd.get_vector(start_point=v4.end_point, angle=angle + 240, length=length, width=3)
+    v5.draw()
+    v6 = sd.line(start_point=point, end_point=v5.end_point, width=3)
+
+
+hexagon(point=start_point_hexagon, angle=angle, length=length)
+#---------------------------------------------------------------------------------------------------------
 
 # Часть 1-бис.
 # Попробуйте прикинуть обьем работы, если нужно будет внести изменения в этот код.
