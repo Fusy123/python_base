91fe87919fe1788279bba27768fb31464e2313e4
user74 <housechel@mail.ru>
2020-09-02 01:16:34 +0500

    Lesson 005 01 - Corrected

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -6,5 +6,6 @@
 import room_1
 import room_2
 
+# TODO А если у нас добавятся жильцы в комнаты, а принты у нас заточены только на 3 жильца! Используйте join
 print('В комнате room_1 живут:', room_1.folks[0], 'и', room_1.folks[1])
 print('В комнате room_2 живут:', room_2.folks[0])
