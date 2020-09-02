7467b1956eb5d339bd20850e8292aaab2e7407ef
Alex <fusy123@mail.ru>
2020-09-02 08:39:03 +0300

    вывод списка жителей района, крректирвока переменных, правка вывода. перенос импорта

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,37 +5,24 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 import textwrap
-# TODO все импорты делаем в начале модуле перед основным кодом программы, пайчарм вам подсказывает об этом
-
-
-folks_list_district = []
-# TODO что есть переменная s ? Нейминг
-s = ', '
 import district.central_street.house1.room1 as room1
-
-folks_list_district.extend(room1.folks)
-
 import district.central_street.house1.room2 as room2
-
-folks_list_district.extend(room2.folks)
-
 import district.central_street.house2.room1 as room3
-
-folks_list_district.extend(room3.folks)
 import district.central_street.house2.room2 as room4
-
-folks_list_district.extend(room4.folks)
 import district.soviet_street.house1.room1 as room5
-
-folks_list_district.extend(room5.folks)
 import district.soviet_street.house1.room2 as room6
-
-folks_list_district.extend(room6.folks)
 import district.soviet_street.house2.room1 as room7
-
-folks_list_district.extend(room7.folks)
 import district.soviet_street.house2.room2 as room8
 
+folks_list_district = []
+
+folks_list_district.extend(room1.folks)
+folks_list_district.extend(room2.folks)
+folks_list_district.extend(room3.folks)
+folks_list_district.extend(room4.folks)
+folks_list_district.extend(room5.folks)
+folks_list_district.extend(room6.folks)
+folks_list_district.extend(room7.folks)
 folks_list_district.extend(room8.folks)
 
-print("На районе живут: ", textwrap.fill(s.join(folks_list_district), width=85))
+print("На районе живут: ", textwrap.fill(', '.join(folks_list_district), width=85))
