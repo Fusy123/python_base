4f962b18578a6ba194ee2198091cf86201928627
user74 <housechel@mail.ru>
2020-09-04 19:01:18 +0500

    Lesson 005 04 - Completed

--- lesson_005/the_country/snowflake.py
+++ lesson_005/the_country/snowflake.py
@@ -1,20 +1,22 @@
 import simple_draw as sd
 
+N = 10
+snowflake_points = []
+
+for i in range(N):
+    x = sd.random_number(0, 200)
+    y = sd.random_number(0, 600)
+    length = sd.random_number(10, 40)
+    factor_a = (sd.random_number(5, 8)) / 10
+    factor_b = (sd.random_number(30, 40)) / 100
+    factor_c = sd.random_number(1, 50)
+    snowflake_points.append([x, y, length, factor_a, factor_b, factor_c])
+
+height_snow = 5
+
 
 def snowflakes():
-    N = 10
-    snowflake_points = []
-
-    for i in range(N):
-        x = sd.random_number(0, 200)
-        y = sd.random_number(0, 600)
-        length = sd.random_number(10, 40)
-        factor_a = (sd.random_number(5, 8)) / 10
-        factor_b = (sd.random_number(30, 40)) / 100
-        factor_c = sd.random_number(1, 50)
-        snowflake_points.append([x, y, length, factor_a, factor_b, factor_c])
-
-    height_snow = 5
+    global height_snow
 
     for i in range(N):
         x = snowflake_points[i][0]
