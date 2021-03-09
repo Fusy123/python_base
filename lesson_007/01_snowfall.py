4aee5311a567f953349fd6010081d1347c215aa6
Alex <fusy123@mail.ru>
2021-03-10 00:02:55 +0300

    падающая снежинка v6

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -13,14 +13,16 @@ sd.resolution = (1200, 600)
 
 class Snowflake:
 
-    def __init__(self):
+    def __init__(self, color=sd.background_color):
+        self.count = 0
         self.x = sd.random_number(0, 1200)
         self.y = sd.random_number(450, 600)
         self.length = sd.random_number(10, 40)
         self.factor_a = (sd.random_number(5, 8)) / 10
         self.factor_b = (sd.random_number(30, 70)) / 100
         self.factor_c = sd.random_number(1, 150)
-        self.color = sd.background_color
+        if color:
+            self.color = color
 
     def move(self):
         self.x += sd.random_number(-30, 30)
@@ -28,7 +30,7 @@ class Snowflake:
 
     def draw(self):
         point = sd.get_point(self.x, self.y)
-        sd.snowflake(center=point, length=self.length, color=sd.COLOR_WHITE, factor_a=self.factor_a,
+        sd.snowflake(center=point, length=self.length, color=self.color, factor_a=self.factor_a,
                      factor_b=self.factor_b, factor_c=self.factor_c)
 
     def clear_previous_picture(self, color=sd.background_color):
@@ -37,28 +39,57 @@ class Snowflake:
                      factor_b=self.factor_b, factor_c=self.factor_c)
 
     def can_fall(self):
-        if self.y > -40:
-            return True
-        else:
-            self.x = sd.random_number(0, 1200)
-            self.y = sd.random_number(450, 600)
-            self.length = sd.random_number(10, 40)
-            self.factor_a = (sd.random_number(5, 8)) / 10
-            self.factor_b = (sd.random_number(30, 70)) / 100
-            self.factor_c = sd.random_number(1, 150)
+        if self.y < -10:
+
+    def __del__(self):
+        if self.y < -10:
+            self.count += 1
 
 
-flake = Snowflake()
+# создать_снежинки(N)
+N = int(input('Сколько снежинок вы хотите создать? :'))
+colors = {'1': ['Красный', sd.COLOR_RED],
+          '2': ['Оранжевый', sd.COLOR_ORANGE],
+          '3': ['Желтый', sd.COLOR_YELLOW],
+          '4': ['Зеленый', sd.COLOR_GREEN],
+          '5': ['Белый', sd.COLOR_WHITE],
+          '6': ['Темно-Зеленый', sd.COLOR_DARK_GREEN],
+          '7': ['Фиолетовый', sd.COLOR_PURPLE]
+          }
+
+print('Выберите цвет: ')
+# вывод соответствия номера - цвету
+for color in colors.items():
+    print(color[0], ': ', color[1][0])
+
+# выбор цвета и проверка правильности ввода
+while True:
+    user_color = input('Введите желаемый цвет: ')
+    if user_color in colors.keys():
+        color_draw = colors[user_color][1]
+        break
+    else:
+        print('Вы ввели неправильный номер цвета!')
+
+flakes = []
+
+for i in range(N):
+    flake = Snowflake(color=color_draw)
+    flakes.append(flake)
 
 while True:
-    flake.clear_previous_picture()
-    flake.move()
-    flake.draw()
-    if flake.can_fall() == False:
-        break
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
+    for flake in flakes:
+        flake.clear_previous_picture()
+        flake.move()
+        flake.draw()
+        flake.can_fall()
+        if flake.can_fall():
+            flake.__del__()
+            flake = Snowflake(color=color_draw)
+            flakes.append(flake)
+        sd.sleep(0.1)
+        if sd.user_want_exit():
+            break
 
 # шаг 2: создать снегопад - список объектов Снежинка в отдельном списке, обработку примерно так:
 # flakes = get_flakes(count=N)  # создать список снежинок
