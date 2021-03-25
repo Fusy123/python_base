5ae98a82e5322b5c9d75bf289b04bb4d8980bc8f
Alex <fusy123@mail.ru>
2021-03-25 21:48:16 +0300

    работа над ошибками

--- lesson_007/03_man_ans_cat.py
+++ lesson_007/03_man_ans_cat.py
@@ -28,6 +28,27 @@ from termcolor import cprint
 # Человеку и коту надо вместе прожить 365 дней.
 
 
+class House:
+    def __init__(self):
+        self.life = []
+        self.food = 50
+        self.money = 100
+        self.cat_food = 0
+        self.mud = 0
+
+    def __str__(self):
+        if len(cats) == 0:
+            return 'В доме еды осталось {}, денег осталось {}'.format(
+                self.food, self.money)
+        else:
+            return 'В доме еды осталось {}, денег осталось {}.\nВ доме осталось кошачей еды {}, уровень грязи {}'.format(
+                self.food, self.money, self.cat_food, self.mud)
+
+    def inspect(self):
+        """ кто живет в доме """
+        return 'В доме живут: ', ', '.join(self.life)
+
+
 class Man:
     '''класс человек. задаем основные параметры и методы'''
 
@@ -35,7 +56,6 @@ class Man:
         self.name = name
         self.fullness = 50
         self.house = None
-        self.life = None
 
     def __str__(self):
         return 'Я - {}, сытость {}'.format(
@@ -93,23 +113,9 @@ class Man:
     def go_to_the_house(self, house):
         ''' метод заселение в дом '''
         self.house = house
-        # TODO у нас будет отдельный метод на эту проверку
-        self.life = 1  # переменная показывающая живой человек в доме (1) или мертвый (0)
+        house.life.append(self.name)
         self.fullness -= 10
-        life.append(citisen.name)  # список живых жителей, сделал для вывода в конце года.
         cprint('{} Въехал(а) в дом'.format(self.name), color='cyan')
-
-    # TODO метод будет изначально принимать экземпляр класса кот
-    def go_to_the_cat_house(self):
-        ''' метод добавления кота в дом'''
-        # TODO тут у на будет такая проверка если у человека определн дом то мы его присвоим принятому коту
-        if len(dead) == 0:  # если список мертвых равен 0, то добавляем кота в дом
-            cat = Cat()  # создаем обьект из класса сат
-            cats.append(cat)  # добавляем обьект в список котов
-            cat.life = 1  # обьекту кот ставим метку живой (1) мертвый (0)
-            life.append(cat.name)  # добавляем имя кота в список живых
-            self.fullness -= 10  # убавляем сытость человека
-            cprint('{} Взяли'.format(cat.name), color='cyan')  # вывод
 
     def man_act(self):
         '''метод активности человека'''
@@ -128,44 +134,31 @@ class Man:
             self.work()
         elif dice == 2:
             self.eat()
-        elif dice == 3:
-            self.go_to_the_cat_house()
+        # elif dice == 3:
+        #     cat.go_to_the_cat_house()
         elif dice == 4:
             self.clean_house()
         else:
             self.play_sega()
 
-    # TODO метод только проверяет никого не удаляем, возвращаем True False
-    def live_dead(self):
-        ''' метод проверки на живой мертвый'''
-        if self.fullness < 0:
-            cprint('{} умер(ла)...'.format(self.name), color='red')
-            dead.append(self.name)
-            life.remove(self.name)
-            citizens.remove(self)
-            return
-
 
 class Cat:
     '''класс кот. основные параметры и методы'''
 
-    def __init__(self):
-        self.name = choice(cat_names)
+    def __init__(self, name):
+        self.name = name
         self.fullness = 50
-        self.house = citisen.house
-        self.life = None
+        self.house = None
 
     def __str__(self):
-        return 'Я - {}, сытость {}'.format(
-            self.name, self.fullness,
-        )
+        return 'Я - {}, сытость {}'.format(self.name, self.fullness)
 
     def eat(self):
         ''' метод еда кота'''
-        if self.house.cat_food >= 10:
+        if citisen.house.cat_food >= 10:
             cprint('{} поел'.format(self.name), color='yellow')
             self.fullness += 20
-            self.house.cat_food -= 10
+            citisen.house.cat_food -= 10
         else:
             self.fullness -= 10
             cprint('{} нет еды'.format(self.name), color='red')
@@ -174,7 +167,7 @@ class Cat:
         ''' метод кот играет '''
         cprint('{} драл обои целый день'.format(self.name), color='green')
         self.fullness -= 10
-        self.house.mud += 5
+        citisen.house.mud += 5
 
     def sleep(self):
         ''' метод кот спит '''
@@ -195,61 +188,57 @@ class Cat:
         else:
             self.sleep()
 
-    def live_dead(self):
-        '''метод проверки жив или нет'''
-        if self.fullness < 0:
-            cprint('{} умер...'.format(self.name), color='red')
-            cat.life = 0
-            dead.append(self.name)
-            life.remove(self.name)
-            cats.remove(self)
-            return
+    def go_to_the_cat_house(self):
+        ''' метод добавления кота в дом'''
+        if citisen.house == my_sweet_home:
+            cat = Cat(name=choice(cats))  # создаем обьект из класса сат
+            cat.house = my_sweet_home
+            my_sweet_home.life.append(self.name)
+            citisen.fullness -= 10  # убавляем сытость человека
+            cprint('Взяли {}'.format(self.name), color='cyan')  # вывод
 
 
-class House:
-    def __init__(self):
-        self.food = 50
-        self.money = 100
-        self.cat_food = 0
-        self.mud = 0
-
-    def __str__(self):
-        if len(cats) == 0:
-            return 'В доме еды осталось {}, денег осталось {}'.format(
-                self.food, self.money)
-        else:
-            return 'В доме еды осталось {}, денег осталось {}.\nВ доме осталось кошачей еды {}, уровень грязи {}'.format(
-                self.food, self.money, self.cat_food, self.mud)
+def live_dead(self):
+    ''' метод проверки на живой мертвый'''
+    if self.fullness < 0:
+        cprint('{} умер(ла)...'.format(self.name), color='red')
+        return True
+    else:
+        return False
 
 
 citizens = [Man(name='Муж'),
             Man(name='Жена')
             ]
 
-cat_names = ['Кот', 'Пушистик', 'Киска', 'Мохнатый ублюдок', 'Облезлый', 'Длинный хвост',
-             'ДИАВОЛ', 'Васька', 'Мурзик', 'Рыжий', 'Одноухий', 'Ушлепок', 'Боксер', 'Р2Д2', 'Мячик']
+# cat_names = ['Кот', 'Пушистик', 'Киска', 'Мохнатый ублюдок', 'Облезлый', 'Длинный хвост',
+#              'ДИАВОЛ', 'Васька', 'Мурзик', 'Рыжий', 'Одноухий', 'Ушлепок', 'Боксер', 'Р2Д2', 'Мячик']
 
-cats = []
-dead = []
-life = []
-# TODO объявляем котов
+cats = [Cat(name='Пушистик'),
+        Cat(name='Мохнатый ублюдок'),
+        Cat(name='Облезлый'),
+        Cat(name='Рыжий'),
+        Cat(name='Ушлепок'),
+        Cat(name='Мурзик')
+        ]
+#
+# dead = []
+# life = []
 
 my_sweet_home = House()
 
 for citisen in citizens:
     citisen.go_to_the_house(house=my_sweet_home)
 
-# TODO заселяем одним человеком котов
+for cat in cats:
+    cat.go_to_the_cat_house()
 
 for day in range(1, 366):  # берем день начиная с 1 по 365 с шагом 1
     print('================ день {} =================='.format(day))
     for citisen in citizens:  # после чего берем жителя из списка жителей
         citisen.man_act()  # производим активность жителя
-        # TODO эту проверку нужно делать в конце цикла, и если что выходить из главного цикла
-        citisen.live_dead()  # проверяем жив он или мертв после активности
     for cat in cats:  # берем кота из списка котов
         cat.cat_act()  # производим активность кота
-        cat.live_dead()  # проверяем жив или мертв после активности
 
     print('--- в конце дня ---')
     for citisen in citizens:  # вывод показателей жителя
@@ -258,13 +247,21 @@ for day in range(1, 366):  # берем день начиная с 1 по 365 с
         print(cat)
     print(my_sweet_home)
     print('')
-    # TODO вот тут делаем проверку. Подумайте как выйти из цикла, остановить его
+    for citisen in citizens:
+        if live_dead(citisen) is True:
+            cprint('{} умер'.format(citisen.name), color='red' )
+            break
+    for cat in cats:
+        if live_dead(cat) is True:
+            cprint('{} умер'.format(cat.name), color='red')
+            break
 
 print('')
-print('Живут в доме: ', end='')
-print(', '.join(map(str, life)))
-print('')
-print('Умер:', ', '.join(map(str, dead)))
+# print('Живут в доме: ', end='')
+# print(', '.join(map(str, life)))
+# print('')
+# print('Умер:', ', '.join(map(str, dead)))
+print(my_sweet_home.inspect())
 
 # Усложненное задание (делать по желанию)
 # Создать несколько (2-3) котов и подселить их в дом к человеку.
