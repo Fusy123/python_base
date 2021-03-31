2f750b904db47b55b9c61f75322cb68c1cd2e0bc
user74 <housechel@mail.ru>
2021-03-31 11:05:56 +0300

    Lesson 007 03 - Corrected

--- lesson_007/03_man_ans_cat.py
+++ lesson_007/03_man_ans_cat.py
@@ -142,6 +142,7 @@ class Man:
         else:
             self.play_sega()
 
+    # TODO только проверка никого не удаляем
     def live_dead(self):
         """ метод проверки на живой мертвый"""
         if self.fullness < 0:
@@ -151,9 +152,16 @@ class Man:
         else:
             return False
 
+    # TODO метод должен только с теми параметрами которые принял на вход.
+    # TODO мы не знаем что такое  cats my_sweet_home cat это все внешние параметры
+
+    # TODO принимаем экземпляр класса кот
     def go_to_the_cat_house(self, name):
         """" метод добавления кота в дом"""
+        # TODO если у человека есть дом то, как это проверить
+        # TODO if self.house:
         if citisen.house == my_sweet_home:
+            # TODO тогда мы этому экземпляру который приняли на вход равняем дом человека
             name.house = my_sweet_home
             cats.append(name)
             my_sweet_home.life.append(cat.name)
@@ -207,6 +215,7 @@ class Cat:
         else:
             self.sleep()
 
+    # TODO метод только для проверки никого не удаляем
     def live_dead(self):
         """ метод проверки на живой мертвый"""
         if self.fullness < 0:
@@ -221,6 +230,7 @@ citizens = [Man(name='Муж'),
             Man(name='Жена')
             ]
 
+# TODO уменьшить до 3х 4х возможно в этом количестве уже будут проблемы
 cat_names = ['Кот', 'Пушистик', 'Киска', 'Мохнатый ублюдок', 'Облезлый', 'Длинный хвост',
              'ДИАВОЛ', 'Васька', 'Мурзик', 'Рыжий', 'Одноухий', 'Ушлепок', 'Боксер', 'Р2Д2', 'Мячик']
 
@@ -233,6 +243,7 @@ for citisen in citizens:
 
 for name in cat_names:
     cat = Cat(name=name)  # создаем обьект из класса сат
+    # TODO интерпритатор не знает о citisen, возьмите 0 элемент из списка citizens
     citisen.go_to_the_cat_house(cat)
     if citisen.fullness == 0:
         break
@@ -256,10 +267,12 @@ for day in range(1, 366):  # берем день начиная с 1 по 365 с
     for citisen in citizens:
         if citisen.live_dead():
             live = True
+            # TODO break можно убрать
             break
     for cat in cats:
         if cat.live_dead():
             live = True
+            # TODO break можно убрать
             break
     if live:
         break
