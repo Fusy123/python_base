fd28a2d128647640e26ecdeaa100c9999e593b08
user74 <housechel@mail.ru>
2021-06-12 14:32:32 +0500

    Lesson 010 02 - Corrected

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -44,6 +44,15 @@ class SuicideError(Exception):
     pass
 
 
+# TODO создайте константу список в котором будут храниться имена class ов чтобы их потом можно было выбрать и вызвать!
+# TODO имена классов храним без их вызовов
+
+# TODO функция one_day() должна возвращать карму от 1 до 7 или рейзит ошибку из расчета 1 к 13
+# TODO мы можем объявить 2 переменные это карма равная рендинт от 1 до 7 и
+# TODO сам еррор который тоже равен рендинт от 1 до 13
+# TODO далее условие если еррор равен 13 то мы choice выбираем случайное исключение из списка
+# TODO и его рейзим как объект используя ()
+# TODO если условие не сработало то мы ретурним карму
 def one_day():
     one_day_carma = randint(1, 8)
     chance_error = randint(1, 14)
@@ -71,11 +80,13 @@ while True:
     try:
         carma += one_day()
         count_day += 1
+        # TODO условие на выход задать в цикле заголовке
         if carma >= ENLIGHTENMENT_CARMA_LEVEL:
             print(f'Добро пожаловать в реальный мир!  Ваша карма {carma}')
             print(f'Вы были у сурка {count_day} дней')
             break
     except Exception as exc:
+        # TODO пишем функцию на запись через with
         file = open('log_error.txt', 'a', encoding='utf8')
         file.write(f'Поймано исключение {exc} ')
         file.write('\n')
