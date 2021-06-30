2f550105ff4ff920d7779fd4cb7e196b25e1b97c
Alex <fusy123@mail.ru>
2021-06-30 16:58:09 +0300

    задание 1 работа над ТОДО

--- lesson_011/01_shapes.py
+++ lesson_011/01_shapes.py
@@ -16,31 +16,28 @@ sd.resolution = (1200, 600)
 
 
 def get_polygon(n):
-    # TODO вот тут пайчарм вам подчеркнул
+    #  вот тут пайчарм вам подчеркнул
+    # TODO что подчеркнул? у меня нет выделенных ошибок
     def draw_triangle(point, angle, length):
         # расчет переменных
-        # TODO попробуйте сократить ряд переменных которые просто переопределены и спользовать можно те которые были
-        # TODO приняты на вход
-        start_point_figure = point
         user_angle = n
-        angle1 = angle
-        length1 = length
         width1 = 5
         color = sd.random_color()
-        point2 = start_point_figure
-        angle_start = angle1
+        point2 = point
+        angle_start = angle
         angle_step = round(360 / user_angle)
         angle_finish = 361 - angle_step
         for angle_draw in range(angle_start, angle_finish, angle_step):  # рисование фигур
-            arm = sd.get_vector(start_point=point2, angle=angle_draw, length=length1, width=width1)
+            arm = sd.get_vector(start_point=point2, angle=angle_draw, length=length, width=width1)
             arm.draw(color=color)
             point2 = arm.end_point
-        sd.line(start_point=start_point_figure, end_point=point2, color=color, width=width1)
+        sd.line(start_point=point, end_point=point2, color=color, width=width1)
 
     return draw_triangle
 
 
-# TODO именование функций не должно пересекаться
+#  именование функций не должно пересекаться
+# TODO именование взято из условия задания как есть.
 draw_triangle = get_polygon(n=8)
 draw_triangle(point=sd.get_point(600, 100), angle=30, length=150)
 
