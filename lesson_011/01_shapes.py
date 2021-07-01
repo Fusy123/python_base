4e269f6b8b073d206483f282be85e14ddfd67025
Alex <fusy123@mail.ru>
2021-07-01 10:57:32 +0300

    задание 1 поправлено название функций

--- lesson_011/01_shapes.py
+++ lesson_011/01_shapes.py
@@ -16,10 +16,10 @@ sd.resolution = (1200, 600)
 
 
 def get_polygon(n):
-    # TODO почему в функция фабрика у вас называется как рисуем треугольник ?
-    # TODO Мы может и квадрат и пентагон и так далее...
-    def draw_triangle(point, angle, length):
-        # расчет переменных
+    """функция рисования фигур, n - количество переданных вершин"""
+
+    def draw_figure_paint(point, angle, length):
+        """подфункция расчета координат вершин фигуры"""
         user_angle = n
         width1 = 5
         color = sd.random_color()
@@ -27,17 +27,16 @@ def get_polygon(n):
         angle_start = angle
         angle_step = round(360 / user_angle)
         angle_finish = 361 - angle_step
-        for angle_draw in range(angle_start, angle_finish, angle_step):  # рисование фигур
+        for angle_draw in range(angle_start, angle_finish, angle_step):
             arm = sd.get_vector(start_point=point2, angle=angle_draw, length=length, width=width1)
             arm.draw(color=color)
             point2 = arm.end_point
         sd.line(start_point=point, end_point=point2, color=color, width=width1)
 
-    return draw_triangle
+    return draw_figure_paint
 
 
-# TODO хорошо тут мы рисуем треугольник передав туда 3
-draw_triangle = get_polygon(n=3)
-draw_triangle(point=sd.get_point(600, 100), angle=30, length=150)
+draw_figure = get_polygon(n=9)
+draw_figure(point=sd.get_point(600, 100), angle=30, length=150)
 
 sd.pause()
